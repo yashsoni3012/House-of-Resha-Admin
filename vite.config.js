b1140bcd695a44c8ac71a91da10f2df1de37e396
Yash <yashranpura3@gmail.com>
2025-12-23 15:53:21 +0530

    23_12_2025

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
+import { defineConfig } from "vite"; // ✅ IMPORT THIS
+import react from "@vitejs/plugin-react"; // if you use React
+
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://api.houseofresha.com',
+      "/api": {
+        target: "https://api.houseofresha.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
